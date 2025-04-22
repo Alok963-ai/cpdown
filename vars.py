5ef63da87f67cc2553e0105894f5228b0ba9344d
Alok963-ai <alokjangde6@gmail.com>
2025-04-22 14:25:26 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,6 +1,6 @@
 api_id = 20288951 #change_with_your_Api_id.
 api_hash = " e8cb5fb7a475b5f5eb3b0ef0e6ca03a8" #change_with_your_Api_hash.
-bot_token = "7827028145:AAGNcXFT5i4rQVHazLXdQqwPtF7D3Hg0ZOM" #change_with_your_bot_token.
+bot_token = "7993028871:AAGq3_hsSxR-0blrZ_yPsXDsB2EBhR1Q0ZI" #change_with_your_bot_token.
 auth_users = [1619650029] #change_with_your_user_id.
 sudo_user = 1713924419 #change_with_your_user_id.
 log_channel = -2610518336 #change_with_your_channel_id.
